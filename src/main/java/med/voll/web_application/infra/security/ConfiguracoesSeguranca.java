47b0599a5d0986f3d9cc4a72fc09e251e554a278
Pedro Bruno Fernandes <pedro.nascimento@maximatech.com.br>
2025-03-15 17:22:56 -0300

    Criado gerenciamento de sessões.

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -3,6 +3,7 @@ package med.voll.web_application.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -42,6 +43,11 @@ public class ConfiguracoesSeguranca {
                         .permitAll())
                 .logout(logout -> logout.logoutSuccessUrl("/login?logout")
                         .permitAll())
+                .rememberMe(rememberMe -> rememberMe.key("lembrarDeMim")
+                        .alwaysRemember(true)
+                        //.tokenValiditySeconds(200000)
+                )
+                .csrf(Customizer.withDefaults())
                 .build();
     }
 }
